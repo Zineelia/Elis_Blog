af61e0e16adf7fcbe559469c8dcf0b5f02271286
Zineelia <elisabeth.hendsel@outlook.com>
2023-04-26 11:40:18 +0200

    initial commit

---
+++ Eli's_blog.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    const link = document.getElementById("target");
+    const hiddenP = document.querySelector(".hidden");
+    console.log(link);
+    console.log(hiddenP);
+
+    link.addEventListener("click", function (event) {
+        hiddenP.classList.remove("hidden");
+        link.classList.add("hidden");
+        event.preventDefault()
+    });
+});
